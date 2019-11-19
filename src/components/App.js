dc2fd1d67456e9aec37bfe0b776d802dbed18c7e
Alias M6 <aliasmontoya719@gmail.com>
2019-11-19 03:26:31 -0700

    setup state to include selected video and create onVideoSelect callback function

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], SelectedVideo: null };
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
@@ -13,12 +13,17 @@ class App extends React.Component {
     });
 
     this.setState({ videos: response.data.items})
-  }
+  };
+
+  onVideoSelect = (video) => {
+    console.log('From the App', video)
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoList onVideoSelect ={this.onVideoSelect} videos={this.state.videos}/>
       </div>
     )
   }
